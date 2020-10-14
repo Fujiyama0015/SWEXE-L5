4c0a470fbd58347b24c1a28196cf0ee580dc6daf
EC2 Default User <ec2-user@ip-172-31-24-92.ec2.internal>
2020-10-14 05:38:37 +0000

    syuuryou

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get 'top/main'
   post 'top/login'
   root 'top#main'
+  get 'top/logout'
+  
+  resources :users, only: [:index, :new, :create, :destroy]
+  
 end
